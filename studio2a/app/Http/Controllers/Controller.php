f27cfdc87a6c703d7b9373948a29ca818a643093
Henry <henry.luu6@hotmail.com>
2023-03-28 21:12:54 +1100

    Add controller.php file

---
+++ studio2a/app/Http/Controllers/Controller.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Laravel\Lumen\Routing\Controller as BaseController;
+use Illuminate\Http\Request;
+
+use Illuminate\Support\Facades\Log;
+
+use App\Models\Test;
+
+class Controller extends BaseController
+{
+    
+}
